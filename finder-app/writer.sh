3499ef7678a8f4b70cf3f6319e9f3f00f9859560
Andrea Fuggetta <afuggetta@gmail.com>
2025-05-06 18:35:39 -0400

    afuggetta : Adding writer.sh

---
+++ finder-app/writer.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# writer.sh created by Andrea Fuggetta for assignment 1
+
+# We are expecting two arguments:
+# - filepath
+# - filecontent
+if [ "$#" -ne 2 ]; then
+    echo "Error: missing parameter(s). You need to provide two arguments."
+    exit 1
+fi
+
+# Store arguments into variables
+FILEPATH=$1
+WRITESTR=$2
+
+DIRPATH=$(dirname "$FILEPATH")
+
+# Directory in $1 has to exist
+if [ -d "$DIRPATH" ]; then
+    echo "$DIRPATH created"
+else
+    echo "Error: Could not create $DIRPATH!"
+    exit 1
+fi
+
+echo "$WRITESTR" >> "$FILEPATH"
+
+# Directory in $1 has to exist
+if [ -f "$FILEPATH" ]; then
+    echo "$FILEPATH created"
+else
+    echo "Error: Failed to create file!"
+    exit 1
+fi
